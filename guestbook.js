e569948831f8dd5e8a90d18b366a86f1c25e3943
Michael-McCune <101901758+Michael-McCune@users.noreply.github.com>
2022-06-02 20:51:05 -0400

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6b68d454.us-south.apigw.appdomain.cloud/guestbook;
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
